a2325fcab13a90d3de2c8e795f036bbc9f3018a4
Ahmed El-Kollaly <ahmed_elkollaly@host.217.53.214.212.nile-online.net>
2017-10-10 15:53:51 +0200

    Initial Commit

---
+++ GrammarChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrammarChecker
+//
+//  Created by Ahmed El-Kollaly on 10/10/17.
+//  Copyright © 2017 Ahmed El-Kollaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
